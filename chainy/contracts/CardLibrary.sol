a184238a60e7e25a9a72387b3b21ed6eb64fec75
eviterin <ethan.viterin@gmail.com>
2025-03-18 19:51:55 +0100

    fixed, hopefully, the reward tree

--- chainy/contracts/CardLibrary.sol
+++ chainy/contracts/CardLibrary.sol
@@ -30,25 +30,25 @@ library CardLibrary {
     // through some path in the reward tree, making all cards obtainable during gameplay.
     function getRewardPair(uint8 lastChosen, uint8 currentFloor) internal pure returns (uint8, uint8) {
         if (currentFloor == 1) {
-            return (CARD_ID_BALANCE, CARD_ID_READ_SCRIPTURE);
-        } else if (lastChosen == CARD_ID_BALANCE) {
-            return (CARD_ID_DIVINE_WRATH, CARD_ID_UNFOLD_TRUTH);
-        } else if (lastChosen == CARD_ID_READ_SCRIPTURE) {
-            return (CARD_ID_DIVINE_WRATH, CARD_ID_PREACH);
-        } else if (lastChosen == CARD_ID_DIVINE_WRATH) {
-            return (CARD_ID_UNVEIL, CARD_ID_SEEK_GUIDANCE);
-        } else if (lastChosen == CARD_ID_UNFOLD_TRUTH) {
-            return (CARD_ID_UNVEIL, CARD_ID_SACRED_RITUAL);
-        } else if (lastChosen == CARD_ID_PREACH) {
-            return (CARD_ID_SEEK_GUIDANCE, CARD_ID_SACRED_RITUAL);
-        } else if (lastChosen == CARD_ID_UNVEIL) {
-            return (CARD_ID_SACRED_RITUAL, CARD_ID_EXPLODICATE);
-        } else if (lastChosen == CARD_ID_SEEK_GUIDANCE) {
-            return (CARD_ID_SACRED_RITUAL, CARD_ID_EXPLODICATE);
-        } else if (lastChosen == CARD_ID_SACRED_RITUAL) {
-            return (CARD_ID_EXPLODICATE, CARD_ID_DIVINE_WRATH);
+            return (CARD_ID_UNFOLD_TRUTH, CARD_ID_BALANCE); // (3, 5)
+        } else if (currentFloor == 2) {
+            return (CARD_ID_PREACH, CARD_ID_SEEK_GUIDANCE); // (4, 8)
+        } else if (currentFloor == 3) {
+            return (CARD_ID_READ_SCRIPTURE, CARD_ID_UNVEIL); // (7, 6)
+        } else if (currentFloor == 4) {
+            return (CARD_ID_SEEK_GUIDANCE, CARD_ID_PREACH); // (8, 4)
+        } else if (currentFloor == 5) {
+            return (CARD_ID_UNVEIL, CARD_ID_BALANCE); // (6, 5)
+        } else if (currentFloor == 6) {
+            return (CARD_ID_READ_SCRIPTURE, CARD_ID_UNFOLD_TRUTH); // (7, 3)
+        } else if (currentFloor == 7) {
+            return (CARD_ID_SACRED_RITUAL, CARD_ID_DIVINE_WRATH); // (9, 10)
+        } else if (currentFloor == 8) {
+            return (CARD_ID_EXPLODICATE, CARD_ID_SACRED_RITUAL); // (11, 9)
+        } else if (currentFloor == 9) {
+            return (CARD_ID_DIVINE_WRATH, CARD_ID_EXPLODICATE); // (10, 11)
         }
-        return (0, 0); // Should never happen
+        return (0, 0); // Fallback, unreachable with 9 rewards (10 floors)
     }
 
     function generateRewards(uint8 lastChosenCard, uint8 currentFloor) internal pure returns (uint8[] memory) {
