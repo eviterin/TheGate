7ff1660439bb8050efc9665d484cbc4a4cf5724e
eviterin <ethan.viterin@gmail.com>
2025-03-20 18:18:50 +0100

    updated some images

--- chainy/contracts/GameState.sol
+++ chainy/contracts/GameState.sol
@@ -144,7 +144,7 @@ contract GameState {
         }
 
         data.runState = RUN_STATE_ENCOUNTER;
-        data.currentFloor = 3; //debug
+        data.currentFloor = 1; //debug
         startEncounter();
     }
 
