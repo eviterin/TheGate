befb09362d12596365e2b80fe11373c505d1eb2a
eviterin <ethan.viterin@gmail.com>
2025-03-20 20:05:40 +0100

    polyfill2

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import './src/polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,7 +19,6 @@ export default defineConfig({
   },
   define: {
     global: 'globalThis',
-    'process.env.NODE_DEBUG': false,
-    'crypto.randomUUID': 'function() { return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) { var r = Math.random() * 16 | 0, v = c == "x" ? r : (r & 0x3 | 0x8); return v.toString(16); }); }'
+    'process.env.NODE_DEBUG': false
   }
 })
