727f67a4a24c3149531911a8ae78d49dd2dafcb6
eviterin <ethan.viterin@gmail.com>
2025-03-20 20:08:29 +0100

    poly3

--- client/src/polyfills.ts
+++ client/src/polyfills.ts
@@ -1,8 +1,17 @@
-if (!crypto.randomUUID) {
-  crypto.randomUUID = function(): `${string}-${string}-${string}-${string}-${string}` {
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0, v = c == "x" ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    }) as `${string}-${string}-${string}-${string}-${string}`;
-  };
+declare global {
+  interface Crypto {
+    randomUUID(): `${string}-${string}-${string}-${string}-${string}`;
+  }
+}
+
+if (typeof crypto !== 'undefined' && !crypto.randomUUID) {
+  Object.defineProperty(crypto, 'randomUUID', {
+    value: function(): `${string}-${string}-${string}-${string}-${string}` {
+      return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == "x" ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      }) as `${string}-${string}-${string}-${string}-${string}`;
+    },
+    configurable: true
+  });
 } 
