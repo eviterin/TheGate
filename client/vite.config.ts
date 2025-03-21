075fa216ce4244c3930fb09352517fee71ab24e2
eviterin <ethan.viterin@gmail.com>
2025-03-21 10:37:07 +0100

    awsstuff

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,9 @@ export default defineConfig({
   assetsInclude: ['**/*.wav'],
   server: {
     host: '0.0.0.0',
-    port: 3000
+    port: 3000,
+    strictPort: true,
+    allowedHosts: ['jasinali.com', 'localhost']
   },
   build: {
     rollupOptions: {
