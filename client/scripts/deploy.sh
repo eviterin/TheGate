b58f397895544ee726d38ca0fc6782e8720a1b5a
eviterin <ethan.viterin@gmail.com>
2025-04-27 00:53:30 +0200

    idk

--- client/scripts/deploy.sh
+++ client/scripts/deploy.sh
@@ -6,5 +6,8 @@ WORKDIR="/home/ec2-user/TheGate"
 
 cd $WORKDIR || exit
 pm2 delete $APP_NAME || true
-pm2 start "$COMMAND" --name $APP_NAME --update-env
+
+# Run the app in dev mode with PM2
+pm2 start "$COMMAND" --name $APP_NAME --update-env --watch
+
 pm2 save
