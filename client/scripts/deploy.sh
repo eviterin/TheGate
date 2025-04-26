b02f36a95feb494ac17c414db17de3182a4ec7a8
eviterin <ethan.viterin@gmail.com>
2025-04-27 00:49:04 +0200

    idk

--- client/scripts/deploy.sh
+++ client/scripts/deploy.sh
@@ -2,7 +2,9 @@
 
 APP_NAME=TheGate
 COMMAND="npm run dev"
+WORKDIR="/home/ec2-user/TheGate"
 
+cd $WORKDIR || exit
 pm2 delete $APP_NAME || true
-pm2 start $COMMAND --name $APP_NAME --update-env
+pm2 start "$COMMAND" --name $APP_NAME --update-env
 pm2 save
